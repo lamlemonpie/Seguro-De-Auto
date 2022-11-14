6759cd87f2ebda037c400de495dee93fd27e3159
lamlemonpie <alejandrolarraondo@gmail.com>
2022-11-14 18:42:00 -0500

    feat: Navbar component with styles

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,10 +1,25 @@
 import React from "react";
+import logoRimac from "../../assets/logo-rimac.png";
+import telephoneImage from "../../assets/phone.png";
 
 export const NavBar = () => {
   return (
     <div className="navbar">
-      <p>¿Tienes alguna duda?</p>
-      <p>(01) 411 6001</p>
+      <img src={logoRimac} alt="Logo Rimac" />
+
+      <div className="navbar__telephone">
+        <p className="navbar__subtitle">¿Tienes alguna duda?</p>
+
+        <img
+          className="navbar__telephone-icon"
+          src={telephoneImage}
+          alt="Phone"
+        />
+
+        <p className="navbar__telephone-number">(01) 411 6001</p>
+
+        <p className="navbar__telephone-quote">Llámanos</p>
+      </div>
     </div>
   );
 };
